519e9a82baef10f1ddcf3e88661d3c1bb64d8150
Christian Saliba <cssaliba@calpoly.edu>
2026-05-02 14:30:40 -0700

    trying sleepunti;

--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
+#include <chrono>
+#include <thread>
 #include <iostream>
-int main(){
-    printf("hello");
-    return 0;
+int main() {
+    using namespace std::this_thread; // sleep_for, sleep_until
+    using namespace std::chrono; // nanoseconds, system_clock, seconds
+    for (int i =0; i < 999; i ++){
+        printf("test %d", i);
+        //sleep_for(nanoseconds(10));
+        sleep_until(system_clock::now() + seconds(1));
+    }
+
 }
