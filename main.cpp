ece99b66b4f43a2f4fb934b39802acd798fc4b81
Christian Saliba <cssaliba@calpoly.edu>
2026-05-02 14:50:50 -0700

    ascii stuff

--- main.cpp
+++ main.cpp
@@ -5,11 +5,19 @@ using namespace std::this_thread; // sleep_for, sleep_until
 using namespace std::chrono; // nanoseconds, system_clock, seconds
 using namespace std;
 int main() {
-    for (int i =0; i < 20; i ++)
-    {
-        cout << " test " << i << endl;
-        //sleep_for(nanoseconds(10));
-        sleep_for(seconds(1));
-    }
+    FILE *procstat = fopen("/proc/stat", "r");
+    int c = fgetc(procstat);
+    int p = fgetc(procstat);
+    int u = fgetc(procstat);
+    int space = fgetc(procstat); 
+    int space2 = fgetc(procstat);
+    int usermode = fgetc(procstat);
+    cout << usermode << endl;
+    // for (int i =0; i < 20; i ++)
+    // {
+    //     cout << " test " << i << endl;
+    //     //sleep_for(nanoseconds(10));
+    //     sleep_for(seconds(1));
+    // }
     return 0;
 }
