f46eb7db0d93d3a3f9fed4d72be10a51056c3418
Christian Saliba <cssaliba@calpoly.edu>
2026-05-02 15:33:11 -0700

    idk what im doing lol

--- main.cpp
+++ main.cpp
@@ -4,25 +4,25 @@
 using namespace std::this_thread; // sleep_for, sleep_until
 using namespace std::chrono; // nanoseconds, system_clock, seconds
 using namespace std;
-long double get_snapshot_total(char *cpu, long double &usertime, long double &nicetime, long double &systime, long double &idletime){
-    FILE *procstat = fopen("/proc/stat", "r");
+long double get_snapshot_total(char *cpu, long double &usertime, long double &nicetime, long double &systime, long double &idletime, FILE *procstat){
+    
     fscanf(procstat, "%s", cpu);
     fscanf(procstat, "%Lf", &usertime);
     fscanf(procstat, "%Lf", &nicetime);
     fscanf(procstat, "%Lf", &systime);
     fscanf(procstat, "%Lf", &idletime);
     long double total = usertime + nicetime + systime + idletime;
-    fclose(procstat);
+    
     return total;
 }
-long double get_snapshot_idle(char *cpu, long double &usertime, long double &nicetime, long double &systime, long double &idletime){
-    FILE *procstat = fopen("/proc/stat", "r");
+long double get_snapshot_idle(char *cpu, long double &usertime, long double &nicetime, long double &systime, long double &idletime, FILE* procstat){
+    
     fscanf(procstat, "%s", cpu);
     fscanf(procstat, "%Lf", &usertime);
     fscanf(procstat, "%Lf", &nicetime);
     fscanf(procstat, "%Lf", &systime);
     fscanf(procstat, "%Lf", &idletime);
-    fclose(procstat);
+    
     return idletime;
 }
 int main() {
@@ -40,11 +40,11 @@ int main() {
     //     //sleep_for(nanoseconds(10));
     //     sleep_for(seconds(1));
     // }
-    long double t1 = get_snapshot_total(cpu, usertime, nicetime, systime, idletime);
-     long double idle_t1 = get_snapshot_idle(cpu, usertime, nicetime, systime, idletime);
+    long double t1 = get_snapshot_total(cpu, usertime, nicetime, systime, idletime, procstat);
+     long double idle_t1 = get_snapshot_idle(cpu, usertime, nicetime, systime, idletime, procstat);
     sleep_for(seconds(1));
-    long double t2 = get_snapshot_total(cpu, usertime, nicetime, systime, idletime);
-    long double idle_t2 = get_snapshot_idle(cpu, usertime, nicetime, systime, idletime);
+    long double t2 = get_snapshot_total(cpu, usertime, nicetime, systime, idletime, procstat);
+    long double idle_t2 = get_snapshot_idle(cpu, usertime, nicetime, systime, idletime, procstat);
     long double delta_time = t2 - t1;
     long double delta_idle = idle_t2 - idle_t1;
 
@@ -53,7 +53,7 @@ int main() {
     cout << cpu_usage << "%" << endl;
     
    
-
+    fclose(procstat);
     
     return 0;
 }
