86831c38b5ff201adba725efd435b8ca27a8826a
sleep-prog <kaihoag6@gmail.com>
2026-05-03 12:03:52 -0700

    pair with pair now

--- main.cpp
+++ main.cpp
@@ -1,17 +1,32 @@
 #include <chrono>
 #include <thread>
 #include <iostream>
-using namespace std::this_thread; // sleep_for, sleep_until
-using namespace std::chrono; // nanoseconds, system_clock, seconds
+#include <utility>
+#include <cstdio>
+
+using namespace std::this_thread;
+using namespace std::chrono;
 using namespace std;
+<<<<<<< HEAD
 long double get_snapshot_total(char *cpu, long double &usertime, long double &nicetime, long double &systime, long double &idletime, FILE *procstat){
     
+=======
+
+pair<long double, long double> get_snapshot(char *cpu, long double &usertime, long double &nicetime, long double &systime, long double &idletime)
+{
+    FILE *procstat = fopen("/proc/stat", "r");
+
+>>>>>>> 77b9783 (pair)
     fscanf(procstat, "%s", cpu);
     fscanf(procstat, "%Lf", &usertime);
     fscanf(procstat, "%Lf", &nicetime);
     fscanf(procstat, "%Lf", &systime);
     fscanf(procstat, "%Lf", &idletime);
+
+    fclose(procstat);
+
     long double total = usertime + nicetime + systime + idletime;
+<<<<<<< HEAD
     
     return total;
 }
@@ -24,15 +39,19 @@ long double get_snapshot_idle(char *cpu, long double &usertime, long double &nic
     fscanf(procstat, "%Lf", &idletime);
     
     return idletime;
+=======
+    return {idletime, total};
+>>>>>>> 77b9783 (pair)
 }
+
 int main() {
-    FILE *procstat = fopen("/proc/stat", "r");
     char cpu[4];
     long double usertime;
     long double nicetime;
     long double systime;
     long double idletime;
 
+<<<<<<< HEAD
     
     // for (int i =0; i < 20; i ++)
     // {
@@ -55,5 +74,26 @@ int main() {
    
     fclose(procstat);
     
+=======
+    pair<long double, long double> snapshot1 = get_snapshot(cpu, usertime, nicetime, systime, idletime);
+
+    long double idle_t1 = snapshot1.first;
+    long double t1 = snapshot1.second;
+
+    sleep_for(seconds(1));
+
+    pair<long double, long double> snapshot2 = get_snapshot(cpu, usertime, nicetime, systime, idletime);
+
+    long double idle_t2 = snapshot2.first;
+    long double t2 = snapshot2.second;
+
+    long double delta_time = t2 - t1;
+    long double delta_idle = idle_t2 - idle_t1;
+
+    long double cpu_usage = 100 * (1 - (delta_idle / delta_time));
+
+    cout << cpu_usage << "%" << endl;
+
+>>>>>>> 77b9783 (pair)
     return 0;
 }
