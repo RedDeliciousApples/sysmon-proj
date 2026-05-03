7c3c2653ea1cec3637f2b5208a0b299fe9565f8a
sleep-prog <kaihoag6@gmail.com>
2026-05-03 12:44:53 -0700

    neat

--- main.cpp
+++ main.cpp
@@ -8,8 +8,14 @@ using namespace std::this_thread;
 using namespace std::chrono;
 using namespace std;
 
-pair<long double, long double> get_snapshot(char *cpu, long double &usertime, long double &nicetime, long double &systime, long double &idletime, long double &iowait, long double &irq, long double &softirq, long double &steal)
+pair<long double, long double> get_snapshot()
 {
+    char cpu[4];
+    long double usertime;
+    long double nicetime;
+    long double systime;
+    long double idletime;
+    long double iowait, irq, softirq, steal;
     FILE *procstat = fopen("/proc/stat", "r");
 
     fscanf(procstat, "%s", cpu);
@@ -48,21 +54,16 @@ long double get_mem_usage(){
 }
 
 int main() {
-    char cpu[4];
-    long double usertime;
-    long double nicetime;
-    long double systime;
-    long double idletime;
-    long double iowait, irq, softirq, steal;
 
-    pair<long double, long double> snapshot1 = get_snapshot(cpu, usertime, nicetime, systime, idletime, iowait, irq, softirq, steal);
+
+    pair<long double, long double> snapshot1 = get_snapshot();
 
     long double idle_t1 = snapshot1.first;
     long double t1 = snapshot1.second;
 
     sleep_for(seconds(1));
 
-    pair<long double, long double> snapshot2 = get_snapshot(cpu, usertime, nicetime, systime, idletime, iowait, irq, softirq, steal);
+    pair<long double, long double> snapshot2 = get_snapshot();
 
     long double idle_t2 = snapshot2.first;
     long double t2 = snapshot2.second;
