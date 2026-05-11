8c2456d16463289590d85d647ec6fe545e909775
Christian Saliba <cssaliba@calpoly.edu>
2026-05-10 17:19:29 -0700

    add json

--- main.cpp
+++ main.cpp
@@ -4,6 +4,21 @@
 #include <utility>
 #include <cstdio>
 
+#include <iostream>
+#include "json.hpp"
+
+using json = nlohmann::json;
+
+int main() {
+    json j;
+    j["cpu"] = 12.4;
+    j["mem"] = 58.2;
+
+    std::cout << j.dump() << std::endl;
+}
+
+using json = nlohmann::json;
+
 using namespace std::this_thread;
 using namespace std::chrono;
 using namespace std;
@@ -47,34 +62,34 @@ long double get_mem_usage(){
 
 }
 
-int main() {
-    char cpu[4];
-    long double usertime;
-    long double nicetime;
-    long double systime;
-    long double idletime;
-    long double iowait, irq, softirq, steal;
+// int main() {
+//     char cpu[4];
+//     long double usertime;
+//     long double nicetime;
+//     long double systime;
+//     long double idletime;
+//     long double iowait, irq, softirq, steal;
 
-    pair<long double, long double> snapshot1 = get_snapshot(cpu, usertime, nicetime, systime, idletime, iowait, irq, softirq, steal);
+//     pair<long double, long double> snapshot1 = get_snapshot(cpu, usertime, nicetime, systime, idletime, iowait, irq, softirq, steal);
 
-    long double idle_t1 = snapshot1.first;
-    long double t1 = snapshot1.second;
+//     long double idle_t1 = snapshot1.first;
+//     long double t1 = snapshot1.second;
 
-    sleep_for(seconds(1));
+//     sleep_for(seconds(1));
 
-    pair<long double, long double> snapshot2 = get_snapshot(cpu, usertime, nicetime, systime, idletime, iowait, irq, softirq, steal);
+//     pair<long double, long double> snapshot2 = get_snapshot(cpu, usertime, nicetime, systime, idletime, iowait, irq, softirq, steal);
 
-    long double idle_t2 = snapshot2.first;
-    long double t2 = snapshot2.second;
+//     long double idle_t2 = snapshot2.first;
+//     long double t2 = snapshot2.second;
 
-    long double delta_time = t2 - t1;
-    long double delta_idle = idle_t2 - idle_t1;
+//     long double delta_time = t2 - t1;
+//     long double delta_idle = idle_t2 - idle_t1;
 
-    long double cpu_usage = 100 * (1 - (delta_idle / delta_time));
+//     long double cpu_usage = 100 * (1 - (delta_idle / delta_time));
 
-    cout << cpu_usage << "%" << endl;
+//     cout << cpu_usage << "%" << endl;
 
-    cout << get_mem_usage() << endl;
+//     cout << get_mem_usage() << endl;
 
-    return 0;
-}
+//     return 0;
+// }
