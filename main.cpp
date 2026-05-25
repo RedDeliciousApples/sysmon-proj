603c09d07f25d4ecee57e09f85c7500569495cbb
Christian Saliba <cssaliba@calpoly.edu>
2026-05-25 16:25:56 -0700

    moving stuff over

--- main.cpp
+++ main.cpp
@@ -15,55 +15,6 @@ using namespace std::this_thread;
 using namespace std::chrono;
 using namespace std;
 
-long double round_to(long double value, int decimals) {
-    long double factor = pow(10, decimals);
-    return round(value * factor) / factor;
-}
-
-pair<long double, long double> get_snapshot()
-{
-    char cpu[4];
-    long double usertime;
-    long double nicetime;
-    long double systime;
-    long double idletime;
-    long double iowait, irq, softirq, steal;
-    FILE *procstat = fopen("/proc/stat", "r");
-
-    fscanf(procstat, "%s", cpu);
-    
-    fscanf(procstat, "%Lf", &usertime);
-    fscanf(procstat, "%Lf", &nicetime);
-    fscanf(procstat, "%Lf", &systime);
-    fscanf(procstat, "%Lf", &idletime);
-    fscanf(procstat, "%Lf", &iowait);
-    fscanf(procstat, "%Lf", &irq);
-    fscanf(procstat, "%Lf", &softirq);
-    fscanf(procstat, "%Lf", &steal);
-
-    fclose(procstat);
-
-    long double total = usertime + nicetime + systime + idletime + iowait + irq + softirq + steal;
-    return {idletime + iowait, total};
-}
-
-long double get_mem_usage(){
-
-
-    long double total;
-    long double memfree;
-    long double available;
-    FILE *meminfo = fopen("/proc/meminfo", "r");
-
-    fscanf(meminfo, "MemTotal: %Lf kB\n", &total);
-    fscanf(meminfo, "MemFree: %Lf kB\n", &memfree);
-    fscanf(meminfo, "MemAvailable: %Lf kB\n", &available);
-
-    fclose(meminfo);
-
-    return round_to(100.0L * (1.0L - available / total), 2);
-
-}
 
 int main() {
 
@@ -89,11 +40,7 @@ int main() {
 
     long double mem_usage = get_mem_usage();
 
-    json j;
-    j["cpu"] = cpu_usage;
-    j["mem"] = mem_usage;
 
-    std::cout << j.dump() << std::endl;
 
     return 0;
 }
--- metrics.cpp
+++ metrics.cpp
@@ -10,3 +10,70 @@
 
 
 using json = nlohmann::json;
+
+using namespace std::this_thread;
+using namespace std::chrono;
+//maybe unsafe to do this?
+using namespace std;
+
+
+long double round_to(long double value, int decimals) {
+    long double factor = pow(10, decimals);
+    return round(value * factor) / factor;
+}
+
+pair<long double, long double> get_cpu_usage()
+{
+    char cpu[4];
+    long double usertime;
+    long double nicetime;
+    long double systime;
+    long double idletime;
+    long double iowait, irq, softirq, steal;
+    FILE *procstat = fopen("/proc/stat", "r");
+
+    //necessary? look into this
+    fscanf(procstat, "%s", cpu);
+    
+    fscanf(procstat, "%Lf", &usertime);
+    fscanf(procstat, "%Lf", &nicetime);
+    fscanf(procstat, "%Lf", &systime);
+    fscanf(procstat, "%Lf", &idletime);
+    fscanf(procstat, "%Lf", &iowait);
+    fscanf(procstat, "%Lf", &irq);
+    fscanf(procstat, "%Lf", &softirq);
+    fscanf(procstat, "%Lf", &steal);
+
+    fclose(procstat);
+
+    long double total = usertime + nicetime + systime + idletime + iowait + irq + softirq + steal;
+    return {idletime + iowait, total};
+}
+
+long double get_mem_usage(){
+
+
+    long double total;
+    long double memfree;
+    long double available;
+    //need to check for failure
+    FILE *meminfo = fopen("/proc/meminfo", "r");
+
+    fscanf(meminfo, "MemTotal: %Lf kB\n", &total);
+    fscanf(meminfo, "MemFree: %Lf kB\n", &memfree);
+    fscanf(meminfo, "MemAvailable: %Lf kB\n", &available);
+
+    fclose(meminfo);
+
+    return round_to(100.0L * (1.0L - available / total), 2);
+
+}
+
+nlohmann::json get_metrics_json() {
+    nlohmann::json j;
+
+    j["cpu"] = get_cpu_usage();
+    j["mem"] = get_mem_usage();
+
+    return j;
+}
