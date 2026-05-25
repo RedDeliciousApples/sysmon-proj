cb17391a793b29a74e5c4e136bd7cd40b9bb3c37
Christian Saliba <cssaliba@calpoly.edu>
2026-05-25 16:45:51 -0700

    json function just calls cpu usage not cpu snapshot

--- metrics.cpp
+++ metrics.cpp
@@ -106,7 +106,7 @@ long double get_cpu_usage(){
 nlohmann::json get_metrics_json() {
     nlohmann::json j;
 
-    j["cpu"] = get_cpu_snapshot();
+    j["cpu"] = get_cpu_usage();
     j["mem"] = get_mem_usage();
 
     return j;
