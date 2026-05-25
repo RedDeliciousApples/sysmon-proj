9f72b6dcfdc14b723b32cf679679c8decd654008
Christian Saliba <cssaliba@calpoly.edu>
2026-05-25 16:21:59 -0700

    maybe split metrics into a new file?

---
+++ metrics.cpp
@@ -0,0 +1,12 @@
+#include <chrono>
+#include <thread>
+#include <iostream>
+#include <utility>
+#include <cstdio>
+#include <cmath>
+
+#include <iostream>
+#include "json.hpp"
+
+
+using json = nlohmann::json;
