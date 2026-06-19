9559ce945a87761fb680c431d46efd8ad3512e2c
sleep-prog <kaihoag6@gmail.com>
2026-06-18 21:56:18 -0700

    uptime

---
+++ src/collectors/uptime.cpp
@@ -0,0 +1,25 @@
+#include <chrono>
+#include <thread>
+#include <iostream>
+#include <utility>
+#include <cstdio>
+#include <cmath>
+#include <stdexcept>
+
+#include <iostream>
+
+long double get_uptime()
+{
+    long double downtime;
+    long double totaltime;
+
+    FILE* uptime_stat = fopen("/proc/uptime", "r");
+
+
+    fscanf(uptime_stat, "%Lf", &downtime);
+    fscanf(uptime_stat, "%Lf", &totaltime);
+
+    long double uptime = (totaltime - downtime) / totaltime * 100;
+    return uptime;
+}
+
