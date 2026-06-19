f0da8723805385773403c071fbcab333ea65f05c
Christian Saliba <cssaliba@calpoly.edu>
2026-06-18 22:01:12 -0700

    organize main, dleete metrics, fix private function

--- src/collectors/cpu.h
+++ src/collectors/cpu.h
@@ -2,5 +2,5 @@
 
 #include <utility>
 
-std::pair<long double, long double> get_cpu_snapshot();
+
 long double get_cpu_usage();
--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,22 @@
 #include <iostream>
-#include "metrics.hpp"
 
+#include "collectors/cpu.h"
+#include "collectors/mem.h"
+#include "utils/getjson.h"
+#include "json.hpp"
 
+int main()
+{
+    std::cout << "CPU usage: "
+              << get_cpu_usage()
+              << "%\n";
 
+    std::cout << "Memory usage: "
+              << get_mem_usage()
+              << "%\n";
 
-int main() {
-        ///////////////////////////
-        //QUICK TESTS
-        ///////////////////////////
-
-        std::cout << "CPU usage: " << get_cpu_usage() << "%\n";
-        std::cout << "Memory usage: " << get_mem_usage() << "%\n";
-
-        std::cout << "JSON:\n";
-        //add .dump(4) for prettier json maybe?
-        std::cout << get_metrics_json() << std::endl;
-
-
+    std::cout << "\nJSON:\n";
+    std::cout << get_metrics_json().dump(4) << '\n';
 
     return 0;
 }
