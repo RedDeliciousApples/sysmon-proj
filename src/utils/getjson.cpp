5fc94bbdd3cfce17c2979cfcc42215ae325e8e32
sleep-prog <kaihoag6@gmail.com>
2026-06-18 22:08:35 -0700

    hope

--- src/utils/getjson.cpp
+++ src/utils/getjson.cpp
@@ -1,6 +1,8 @@
 #include "json.hpp"
 #include "../collectors/cpu.h"
 #include "../collectors/mem.h"
+#include "../collectors/uptime.h"
+
 
 nlohmann::json get_metrics_json()
 {
@@ -8,6 +10,6 @@ nlohmann::json get_metrics_json()
 
     j["cpu"] = get_cpu_usage();
     j["mem"] = get_mem_usage();
-
+    j["awake"] = time_awake();
     return j;
 }
