60eebad9533eb3d9a4891618b092132fb55382cf
sleep-prog <kaihoag6@gmail.com>
2026-06-21 11:18:44 -0700

    added watch

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <unistd.h>
+
 
 #include "collectors/cpu.h"
 #include "collectors/mem.h"
@@ -7,8 +9,32 @@
 #include "utils/getjson.h"
 #include "json.hpp"
 
-int main()
+int main(int argc, char** argv)
 {
+    bool watching = false;
+    if (argc > 1)
+    {
+        if(strcmp(argv[1], "--watch") == 0)
+            watching = true;
+    }
+
+    if(watching)
+    {
+        while(true)
+        {
+            std::cout << "CPU usage: "
+                << get_cpu_usage()
+                << "%\n";
+
+            std::cout << "Memory usage: "
+                << get_mem_usage()
+                << "%\n";
+            std::cout << "uptime in seconds:" << time_awake() << " seconds \n";
+            std::cout << "\nJSON:\n";
+            std::cout << get_metrics_json().dump(4) << '\n';
+            sleep(1);                
+        }
+    }
     std::cout << "CPU usage: "
               << get_cpu_usage()
               << "%\n";
