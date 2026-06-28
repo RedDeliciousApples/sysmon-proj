89642f1fe8768b736bf6219b69117785908e5814
Christian Saliba <cssaliba@calpoly.edu>
2026-06-28 15:17:56 -0700

    hopefully works? need to handle --serve argument later

--- src/server/http_server.cpp
+++ src/server/http_server.cpp
@@ -11,5 +11,37 @@
 void run_server(int port)
 {
     int server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    std::cout << "Server would start on port " << port << "\n";
+
+    if (server_fd < 0) {
+        throw std::runtime_error("Failed to create socket");
+    }
+
+    int opt = 1;
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0) {
+        close(server_fd);
+        throw std::runtime_error("Failed to set socket options");
+    }
+
+    sockaddr_in address{};
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(port); //convert to network byte order
+
+    if (bind(server_fd, reinterpret_cast<sockaddr*>(&address), sizeof(address)) < 0) {
+        close(server_fd);
+        throw std::runtime_error("Failed to bind socket");
+    }
+
+    if (listen(server_fd, 10) < 0) {
+        close(server_fd);
+        throw std::runtime_error("Failed to listen on socket");
+    }
+
+    std::cout << "sysmon server listening on http://localhost:" << port << "\n";
+
+    while (true) {
+        // handle client and stuff
+    }
+
+    close(server_fd);
 }
