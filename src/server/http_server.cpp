5c94072590513df9b990d64f2d3143992bb13c39
Christian Saliba <cssaliba@calpoly.edu>
2026-06-28 15:01:04 -0700

    add some basiuc server scaffolding

--- src/server/http_server.cpp
+++ src/server/http_server.cpp
@@ -1,8 +1,15 @@
 #include "http_server.h"
 
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
 #include <iostream>
+#include <stdexcept>
 
 void run_server(int port)
 {
+    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
     std::cout << "Server would start on port " << port << "\n";
 }
